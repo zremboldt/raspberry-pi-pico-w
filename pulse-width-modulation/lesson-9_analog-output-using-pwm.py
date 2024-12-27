737da0e0f5437da74b59f8fbd28b18996537dfd0
zremboldt <zac.remboldt@joinroot.com>
2024-12-27 14:36:00 -0600

    Completed lesson 9

--- pulse-width-modulation/lesson-9_analog-output-using-pwm.py
+++ pulse-width-modulation/lesson-9_analog-output-using-pwm.py
@@ -1,16 +1,16 @@
 from machine import Pin, PWM
-from time import sleep
 
-outPin = 16
-analogOut = PWM(Pin(outPin))
-analogOut.freq(1000)
-analogOut.duty_u16(0)
+outPin = 11
+analogOutPin = PWM(Pin(outPin)) # Set the pin as a PWM output
+analogOutPin.freq(1000) # Set the frequency of the PWM signal to 1000Hz. This will blink once per millisecond. Play with the value if desired.
+analogOutPin.duty_u16(0) # Set the duty cycle of the PWM signal. This is a 16 bit binary number. 0 is off and 65535 is fully on.
 
 while True:
-  binaryMax = 65550 
-  desiredMax = 100 # We want to convert the binary value to a number between 0 and 100
+  binaryMax = 65550
+  humanReadableMax = 100 # We want to convert the binary value to a number between 0 and 100
+  
   userInput = int(input("Enter a number between 0 and 100: "))
 
-  outputNumber = (binaryMax / 100) * userInput
-
+  outputNumber = round((binaryMax / humanReadableMax) * userInput)
+  analogOutPin.duty_u16(outputNumber)
   print(outputNumber)

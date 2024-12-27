955bca2d9777ef3e7ea9d2b0b9bcde6990f2e3db
zremboldt <zac.remboldt@joinroot.com>
2024-12-26 21:15:16 -0600

    .

--- pulse-width-modulation/lesson-9_analog-output-using-pwm.py
+++ pulse-width-modulation/lesson-9_analog-output-using-pwm.py
@@ -7,4 +7,10 @@ analogOut.freq(1000)
 analogOut.duty_u16(0)
 
 while True:
-  
+  binaryMax = 65550 
+  desiredMax = 100 # We want to convert the binary value to a number between 0 and 100
+  userInput = int(input("Enter a number between 0 and 100: "))
+
+  outputNumber = (binaryMax / 100) * userInput
+
+  print(outputNumber)
